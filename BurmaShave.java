e4b17109294385a454d04dce5f7525dda3cce0b4
24024118-lang <tokologomagoma5@gmail.com>
2025-04-04 10:55:53 +0200

    Add files via upload

---
+++ BurmaShave.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.burmashave;
+
+/**
+ *
+ * @author 24024118
+ */
+import javax.swing.JOptionPane;
+public class BurmaShave {
+
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null,"Shaving brushes");
+       JOptionPane.showMessageDialog(null,"You'll soon seem 'em");
+       JOptionPane.showMessageDialog(null,"On a shelf");
+       JOptionPane.showMessageDialog(null,"In some museum args");
+       
+       
+    }
+}
